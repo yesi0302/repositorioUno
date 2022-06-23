dd1574660173dfbd56feed23f68744b1c305b423
yesi0302 <yesi.islas.rive.m.22@gmail.com>
2022-06-23 11:58:41 -0500

    pruebas

---
+++ pagina 1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace proyectoCholamundoYesenia
+{
+    public partial class pagina1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "Hola mundo..";
+        }
+    }
+}
